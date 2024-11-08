e127a747fd4afeb200c3b417b0568c4008613ec3
Pranav Deshmukh <pranavdeshmukh190@gmail.com>
2024-11-08 10:23:40 +0530

    server init

---
+++ server/src/index.ts
@@ -0,0 +1,17 @@
+import express, { Request, Response } from 'express';
+
+
+const app = express();
+const port = process.env.PORT || 8000;
+
+app.use(express.json());
+
+
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello, TypeScript with Express!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
